22bfc0ac247e33ef6cc31d25cb5d8611ef5f65d0
ГЕОРГИЙ БЕКЛЕМИШЕВ <111ag@rambler.ru>
2021-05-27 02:34:29 +0300

    v0.2

---
+++ GameQuest/Classes/NodeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameQuest.Classes
+{
+    class NodeController
+    {
+        public static List<Node> nodeList = new List<Node>();
+        TextBox textWindow = Application.OpenForms["MainForm"].Controls["outputTextBox"] as TextBox;
+        private Boolean printNextMessage = false;
+
+        public async void nextNode(string node)
+        {
+            Panel mainPanel = null;
+            PictureBox mainPicture = null;
+            textWindow.Click += nextMessage_Click;
+            Node currentNode = nodeList.Find(x => x.name == node);
+            textWindow.Text = "";
+
+            foreach (Control control in Application.OpenForms["MainForm"].Controls)
+            {
+                if (control is Panel && control.Name == "mainPanel")
+                    mainPanel = control as Panel;
+                if (control is PictureBox && control.Name == "mainPicture")
+                {
+                    mainPicture = control as PictureBox;
+                    mainPicture.Click += nextMessage_Click;
+                }
+            }
+            mainPanel.Controls.Clear();
+
+
+                     
+
+            for (int i = 0; i < currentNode.lines.Count; i++)
+            {
+                textWindow.Text = "";
+                if (currentNode.lines[i].image != null)
+                    mainPicture.Image = currentNode.lines[i].image;
+
+                await TextTyper.Typer(textWindow, currentNode.lines[i].text);
+
+                while (!printNextMessage)
+                {
+                    await Task.Delay(10);                    
+                }
+                printNextMessage = false;
+                
+            }
+
+            int step = 0;
+            foreach (var gate in nodeList.Find(x => x.name == node).gates)
+            {
+                if (gate.condition != null && !InventoryController.PlayerInventory.Contains(InventoryController.ItemsList.Find(x => x.itmName == gate.condition)))
+                {
+                    continue;
+                }
+
+                Label btn1 = new Label();
+                btn1.Location = new System.Drawing.Point(10, 10 + step);
+                btn1.Tag = gate.LinkedNode.name;
+                btn1.AutoSize = true;                
+                btn1.Text = gate.action;                
+                step += btn1.Height + 5;
+                btn1.Click += nextNode_Click;
+                Application.OpenForms["MainForm"].Controls.Add(btn1);
+                btn1.Parent = mainPanel;
+            }
+            
+        }
+
+        private void nextMessage_Click(object sender, EventArgs e)
+        {
+            printNextMessage = true;
+        }
+
+        private void nextNode_Click(object sender, EventArgs e)
+        {
+            nextNode((sender as Label).Tag.ToString());
+        }
+    }
+}
---
+++ GameQuest/MainForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace GameQuest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPicture = new System.Windows.Forms.PictureBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPicture
+            // 
+            this.mainPicture.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.mainPicture.Location = new System.Drawing.Point(12, 12);
+            this.mainPicture.Name = "mainPicture";
+            this.mainPicture.Size = new System.Drawing.Size(720, 480);
+            this.mainPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mainPicture.TabIndex = 0;
+            this.mainPicture.TabStop = false;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.outputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputTextBox.Location = new System.Drawing.Point(12, 498);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTextBox.ShortcutsEnabled = false;
+            this.outputTextBox.Size = new System.Drawing.Size(720, 105);
+            this.outputTextBox.TabIndex = 1;
+            this.outputTextBox.TabStop = false;
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.mainPanel.Location = new System.Drawing.Point(12, 609);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(720, 119);
+            this.mainPanel.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 733);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.mainPicture);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mainPicture;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Panel mainPanel;
+    }
+}
+
